52f2f3f823768e02b6239fcffb0e981f2cb94d4b
nhh2111 <nghuyhoang211104@gmail.com>
2026-03-30 10:39:22 +0700

    Cập nhật link API Render, chuẩn bị deploy Vercel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api' // Đường dẫn trỏ tới Backend Golang
+  apiUrl: 'https://api-nhatro-backend.onrender.com' // Đường dẫn trỏ tới Backend Golang
 };
