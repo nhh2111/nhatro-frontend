01c5b5b29908a68fb8e66610eaacb4bf40fe0044
nhh2111 <nghuyhoang211104@gmail.com>
2026-04-06 14:59:48 +0700

    Thêm tính năng A

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://api-nhatro-backend.onrender.com/api'
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'https://api-nhatro-backend.onrender.com/api'
+  // apiUrl: 'http://localhost:8080/api'
 };
