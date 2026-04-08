dc6d172b5b7b908f1f4fa388356ef640b4d914d7
nhh2111 <nghuyhoang211104@gmail.com>
2026-04-08 15:36:25 +0700

    sua loi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://api-nhatro-backend.onrender.com/api' // Đường dẫn trỏ tới Backend Golang
+  // apiUrl: 'https://api-nhatro-backend.onrender.com/api' // Đường dẫn trỏ tới Backend Golang
   // test local
-  // apiUrl: 'http://127.0.0.1:8080/api'
+  apiUrl: 'http://127.0.0.1:8080/api'
 };
